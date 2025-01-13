cdf72e367a3b8eb6b1744c0864c24d4656cf2d86
Arun <Arun@12>
2025-01-13 08:01:38 +0545

    Getter and setter method is write in student.java

---
+++ student.java
@@ -0,0 +1,21 @@
+public class Student{
+    public String name;;
+    private int age;
+    public Students(String name, int age){
+        this.name = name;
+        this.age = age;
+
+    }
+    public String getName(){
+        return name;
+    }
+    void Setname(String name){
+        this.name = name;
+    }
+    public int setAge(){
+        return age;
+    }
+    void getAge(int age){
+        this.age = age;
+    }
+}

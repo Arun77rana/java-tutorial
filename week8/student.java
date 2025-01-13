dd7b48c136a5894659617e50a89fc20d7692a675
Arun <Arun@12>
2025-01-13 07:36:33 +0545

    File are change

---
+++ week8/student.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class student{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+    private String name;
+    private int age;
+    private int rollno;
+    private String branch;
+   
+}
+    
